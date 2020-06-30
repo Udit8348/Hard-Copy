d5b441f2890a7515bdcd21c4bf9251c26f307ef3
Udit S <udit610@yahoo.com>
2020-06-30 03:30:53 -0400

    extensive comments and added success msg

--- scripts/prosToMinted.py
+++ scripts/prosToMinted.py
@@ -4,13 +4,9 @@ import glob
 import zipfile
 from shutil import copy, rmtree
 
-# function that zips a selected directory
+# Function which zips the given 'path' to the 'ziph'
 def zipdir(path, ziph):
-    # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
-        print("root is ", root)
-        print("dirs is ", dirs)
-        print("files is ", files)
         for file in files:
             ziph.write(os.path.join(root, file))
 
@@ -19,21 +15,27 @@ def main(dest):
         os.mkdir(dest)
     except OSError as e:
         print(e)
-    try:
-        os.mkdir(dest)
-    except OSError as e:
-        print(e)
-    with open("codeToLaTeX.tex", "w+") as f:
+
+    # Create a 'Code' LaTeX file
+    # This is responsible for reading a copy of the valid files
+    # then, formatting their contents
+    with open("code.tex", "w+") as f:
+        # Recursively search the entire project for any Header Files
         f.write("%%-----------------------------------------\n")
         f.write("\\section{Header Files}\n\n")
         for header in glob.glob('**/*.h*', recursive=True):
-            if "api.h" in header or "/display/" in header or "/Eigen/" in header or "/okapi/" in header or "/pros/" in header:
+            # Exclude PROS API headers
+            if ("api.h" in header) or ("/display/" in header) or ("/okapi/" in header) or ("/pros/" in header):
                 continue
+
+            # Generate LaTeX code for any valid header
+            # Write it to the 'Code' LaTeX file
             f.write("\\subsection{" + header + "}\n")
             f.write("\\inputminted[linenos,tabsize=2,breaklines, breakanywhere]{c}{" + header + "}\n")
             f.write("\\pagebreak\n\n")
+
+            # Copy the file's tree into the destination
             head, tail = os.path.split(header)
-            print("This is the head: ", head)
             path = dest + "/" + head
             try:
                 os.mkdir(path)
@@ -42,43 +44,59 @@ def main(dest):
                 print(e)
             copy(header, path)
 
+        # Recursively search the entire project for any Source Files
         f.write("%%-----------------------------------------\n")
         f.write("\\section{Source Files}\n\n")
-
         for source in glob.glob('**/*.c*', recursive=True):
+            # Exlude any object files or csv files
             if ".o" in source or ".csv" in source:
                 continue
+
+            # Generate LaTeX code for any valid source file
+            # Write it to the 'Code' LaTeX file
             f.write("\\subsection{" + source + "}\n")
             f.write("\\inputminted[linenos,tabsize=2,breaklines, breakanywhere]{c}{" + source + "}\n")
             f.write("\\pagebreak\n\n")
+
+            # Copy the file's tree into the destination
             head, tail = os.path.split(source)
-            print("This is head: ", head)
             path = dest + "/" + head
-            print("This is path: ", path)
             try:
                 os.mkdir(path)
             except OSError as e:
                 print(e)
             copy(source, path)
 
-    copy("codeToLaTeX.tex", dest)
-    os.remove("codeToLaTeX.tex")
+    # The 'Code' LaTeX file is completely written now
+    # Save a copy of it to the destination and remove the original
+    copy("code.tex", dest)
+    os.remove("code.tex")
+
+    # Create the 'Main' LaTeX file
+    # This is the container for the 'Code' LaTeX file
+    # It contains document specific formatting and information
     with open("main.tex", "w+") as f:
-        print("Reached Conversion")
-        f.write("\\documentclass{article}\n\\usepackage[utf8]{inputenc}\n\\usepackage[margin=1in]{geometry}\n\\title{PSU Unified Source Code}\n\\author{subramanyau}\n\\date{June 2020}\n\\usepackage{minted}\n\\begin{document}\n\\input{codeToLaTeX.tex}\n\n\\end{document}")
+        f.write("\\documentclass{article}\n\\usepackage[utf8]{inputenc}\n\\usepackage[margin=1in]{geometry}\n\\title{PROJECT_TITLE}\n\\author{LAST_FIRST}\n\\date{MONTH_YEAR}\n\\usepackage{minted}\n\\begin{document}\n\\input{code.tex}\n\n\\end{document}")
+
+    # The 'Main' LaTeX file is completely written now
+    # Save a copy of it to the destination and remove the original
     copy("main.tex", dest)
     os.remove("main.tex")
 
-    # zip the tex folder so it can be uploaded to overleaf
+    # Zip the contents found at the destination
+    # A Zip file can be directly uploaded to overleaf
+    # @TODO might need to use regex to strip the name from the dest to create the path name for the zip
     zipf = zipfile.ZipFile("docs/Tex.zip", 'w', zipfile.ZIP_DEFLATED)
     zipdir(dest, zipf)
     zipf.close()
 
-
+    # Delete the folder at the destination; a zip copy of it has been created
     try:
         rmtree(dest) #removes folder and all contained items
     except OSError as e:
         print("Error: %s : %s" % (dir_path, e.strerror))
+
+    print(">>> Success! Find your zip at:", dest)
 
 if __name__ == "__main__":
     main(sys.argv[1])
